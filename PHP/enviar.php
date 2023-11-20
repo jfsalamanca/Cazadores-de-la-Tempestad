b2176a25f2e7875770897f8b3c701a56d4ffce35
jfsalamanca <95266502+jfsalamanca@users.noreply.github.com>
2023-11-19 21:55:49 -0500

    Add files via upload

---
+++ PHP/enviar.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $nombre = $_POST["nombre"];
+    $email = $_POST["email"];
+    $mensaje = $_POST["mensaje"];
+
+    $destinatario = "john.salamanca@cun.edu.co"; // Reemplaza con tu dirección de correo electrónico
+    $asunto = "Mensaje de contacto de Cazadores de laTempestad";
+
+    $mensaje_email = "Nombre: " . $nombre . "\n";
+    $mensaje_email .= "Correo Electrónico: " . $email . "\n";
+    $mensaje_email .= "Mensaje:\n" . $mensaje;
+
+    // Envía el correo
+    mail($destinatario, $asunto, $mensaje_email);
+
+    // Redirige a una página de confirmación
+    header("Location: confirmacion.html");
+}
